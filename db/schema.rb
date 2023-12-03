e41e033b24f3e3e24b64c4cb042a48a9a8b40b72
EC2 Default User <ec2-user@ip-172-31-22-182.ap-northeast-1.compute.internal>
2023-12-03 18:01:17 +0000

    Liveモデルの作成、マイグレーションでnameカラムの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_12_03_145411) do
+ActiveRecord::Schema.define(version: 2023_12_03_173822) do
+
+  create_table "lives", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.integer "user_id"
