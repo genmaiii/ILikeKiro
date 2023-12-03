cd6396df57742232679cab8e242b46412aee08e5
EC2 Default User <ec2-user@ip-172-31-22-182.ap-northeast-1.compute.internal>
2023-12-03 19:06:20 +0000

    resourcesでルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,7 @@
 Rails.application.routes.draw do
-  get 'lives/index'
-  get 'lives/new'
-  get 'lives/edit'
-  get 'posts/index'
-  get 'posts/show'
-  get 'posts/new'
-  get 'posts/edit'
-  get 'users/show'
-  get 'users/edit'
   devise_for :users
   root to: 'homes#top'
+  resources :users, only: [:show, :edit, :update]
+  resources :posts
+  resources :lives, only: [:index, :new, :create, :edit, :update, :destroy]
 end
