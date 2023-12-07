909a53e910cb2a67a8c7ef09f85a47d3575ab1bd
EC2 Default User <ec2-user@ip-172-31-22-182.ap-northeast-1.compute.internal>
2023-12-07 17:08:42 +0000

    devise日本語化パケのインストールと日本語ファイルの生成・使用の設定

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,10 @@ class Post < ApplicationRecord
   belongs_to :user
   belongs_to :live, foreign_key: :live_name_id
   
+  #validates :live_date, presence: true
+  #validates :venue, presence: true
+  #validates :live_name_id, presence: true
+  #validates :category, presence: true
   validates :comment, presence: true
 
 end
