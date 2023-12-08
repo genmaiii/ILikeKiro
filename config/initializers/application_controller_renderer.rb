f703f4f3d508554881be6ee06b2a912a537d8f6e
EC2 Default User <ec2-user@ip-172-31-22-182.ap-northeast-1.compute.internal>
2023-12-08 13:50:38 +0000

    ユーザーのアイコンの設定、変更機能を実装

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'dc4b0f21587805bfc4921db3bb02c90c5910bb9b1d738d2583afdbe1231805c9c4bc8787b1a94e73c110cca6ab3e410ff5dac28567c833d2115345ce8e32fc87'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_12_03_180256) do
+ActiveRecord::Schema.define(version: 2023_12_08_124558) do
 
   create_table "lives", force: :cascade do |t|
     t.string "name"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 2023_12_03_180256) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "profile_image_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
