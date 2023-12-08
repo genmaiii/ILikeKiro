71118b1c3b6af1065535707664148721a137fc64
EC2 Default User <ec2-user@ip-172-31-22-182.ap-northeast-1.compute.internal>
2023-12-08 12:43:25 +0000

    ユーザー詳細・編集画面の修正、編集機能の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,25 @@
 class UsersController < ApplicationController
   def show
+    @user = User.find(params[:id])
   end
 
   def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+       redirect_to user_path(@user.id)
+       flash[:notice] = "プロフィールを変更しました"
+    else
+      render :edit
+    end
+  end
+  
+  private
+
+  def user_params
+    params.require(:user).permit(:name)
   end
 end
