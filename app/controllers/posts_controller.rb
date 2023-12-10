e08954ea812f3786087254b9c71edc29b54ce519
EC2 Default User <ec2-user@ip-172-31-22-182.ap-northeast-1.compute.internal>
2023-12-10 17:42:35 +0000

    postコントローラーに投稿機能のアクション、ストロングパラメーターを記述

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,8 +6,27 @@ class PostsController < ApplicationController
   end
 
   def new
+    @post = Post.new
+  end
+  
+  def create
+    @post = Post.new(post_params)
+    @post.save
+    if @post.save
+      redirect_to post_path(@post)
+      flash[:notice] = '投稿が送信されました'
+    else
+      render :index
+    end
   end
 
   def edit
   end
+  
+  
+  private
+  def post_params
+    params.require(:post).permit(:live_date, :venue, :category, :comment)
+  end
+
 end
