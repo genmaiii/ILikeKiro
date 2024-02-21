1f50ebff2fdfe5c921ddb5365555fd5b4985f896
EC2 Default User <ec2-user@ip-172-31-22-182.ap-northeast-1.compute.internal>
2024-02-21 17:48:32 +0000

    細かい修正など

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,16 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def new
     @post = Post.new
   end
-  
+
   def create
     @post = Post.new(post_params)
     @post.user_id = current_user.id
@@ -17,14 +19,16 @@ class PostsController < ApplicationController
       flash[:notice] = '投稿が送信されました'
     else
       render :index
+      flash[:notice] = '投稿に失敗しました'
     end
   end
 
   def edit
   end
-  
-  
+
+
   private
+  
   def post_params
     params.require(:post).permit(:live_date, :venue, :category, :live_name, :comment)
   end
