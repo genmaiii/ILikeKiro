21edbae120bb49d95a1a35cd8555e9f2a76b2490
EC2 Default User <ec2-user@ip-172-31-22-182.ap-northeast-1.compute.internal>
2024-03-07 19:11:22 +0000

    rails_admin、cancancanの追加、セットアップ

--- Gemfile
+++ Gemfile
@@ -73,3 +73,6 @@ gem 'font-awesome-sass', '~> 5.13'
 
 gem "refile", require: "refile/rails", github: 'manfe/refile'
 gem "refile-mini_magick"
+
+gem 'rails_admin'
+gem 'cancancan'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_01_07_125117) do
+ActiveRecord::Schema.define(version: 2024_03_07_184630) do
 
   create_table "lives", force: :cascade do |t|
     t.string "name"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 2024_01_07_125117) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "profile_image_id"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
