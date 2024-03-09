8d81de69246a5e994451cf90241464ad0128de6f
EC2 Default User <ec2-user@ip-172-31-22-182.ap-northeast-1.compute.internal>
2024-03-09 18:20:11 +0000

    ユーザー名でのログインの実装、その他細かい修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,19 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+  attachment :profile_image
   
   has_many :posts, dependent: :destroy
   
-  attachment :profile_image
+  validates :name, presence: true, uniqueness: true, length: {in: 2..20 }
+  validates :email, uniqueness: true
+  
+  def email_required?
+    false
+  end
+  
+  def email_changed?
+    false
+  end
 end
