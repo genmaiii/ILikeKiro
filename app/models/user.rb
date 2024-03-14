8ed086cd96b17fdfc6022606d744b68947170eb7
EC2 Default User <ec2-user@ip-172-31-22-182.ap-northeast-1.compute.internal>
2024-03-14 18:17:37 +0000

    バリデーション、エラーメッセージの修正、その他細かい修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,8 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   
   validates :name, presence: true, uniqueness: true, length: {in: 2..20 }, format: { with: /\A[\w]+\z/, message: "は半角英数字、アンダーバーのみ使用できます" }
-  validates :display_name, presence: true, uniqueness: true, length: {in: 2..20 }
+  validates :display_name, presence: true, length: {in: 2..20 }
+  validates :password, presence: true, length: {in:6..128 }, format: { with: /\A[\w]+\z/ }
   validates :email, uniqueness: true
   
   def email_required?
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
   root to: 'homes#top'
-  resources :users, only: [:show, :edit, :update]
+  resources :users, only: [:index, :show, :edit, :update]
   resources :posts
   resources :lives, only: [:index, :new, :create, :edit, :update, :destroy]
 end
